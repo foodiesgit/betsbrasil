379b9bd0353309e8e3ca7f9a8ed0056a20db27ab
Victor Hugo <victorhugoraj@gmail.com>
2021-05-28 20:47:33 -0300

    Update include_sbet.blade.php

--- resources/views/client/include_sbet.blade.php
+++ resources/views/client/include_sbet.blade.php
@@ -83,7 +83,7 @@
                   <ul class="rd-navbar-nav"> 
                     <li class="rd-nav-item active"><a class="rd-nav-link" href="/">Início</a>
                     </li>
-                    <li class="rd-nav-item"><a class="rd-nav-link" href="/jogos-ao-vivo">Ao Vivo</a>
+                    <li class="rd-nav-item"><a class="rd-nav-link" href="/jogos-ao-vivo">Ao Vivo</a></li>
                     <li class="rd-nav-item d-lg-block d-lg-none d-xl-block d-xl-none" ><a class="rd-nav-link" href="/data/{{\Carbon\Carbon::now()->addDay(1)->locale('pt_BR')->format('Y-m-d')}}">Jogos do dia {{\Carbon\Carbon::now()->addDay(1)->locale('pt_BR')->format('d/m/Y')}}</a></li>
                     <li class="rd-nav-item d-lg-block d-lg-none d-xl-block d-xl-none"><a class="rd-nav-link" href="/data/{{\Carbon\Carbon::now()->addDay(2)->locale('pt_BR')->format('Y-m-d')}}">Jogos do dia {{\Carbon\Carbon::now()->addDay(2)->locale('pt_BR')->format('d/m/Y')}}</a></li>
                     <li class="rd-nav-item d-lg-block d-lg-none d-xl-block d-xl-none"><a class="rd-nav-link" href="/data/{{\Carbon\Carbon::now()->addDay(3)->locale('pt_BR')->format('Y-m-d')}}">Jogos do dia {{\Carbon\Carbon::now()->addDay(3)->locale('pt_BR')->format('d/m/Y')}}</a></li>
