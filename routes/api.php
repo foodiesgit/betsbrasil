50e719e1b0b3d34d58083f3faffab369f14ffce1
Victor Hugo <victorhugoraj@gmail.com>
2021-05-31 20:59:55 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::prefix('v1')->group(function(){
     Route::middleware('auth:api')->group(function(){
         Route::get('recupera-cambista', 'ApiAndroidController@recuperaCambista');
         Route::get('recupera-carrinho', 'ApiAndroidController@recuperarCarrinho');
-        Route::get('remove-selection/{id},', 'ApiAndroidController@removeCarrinho');
+        Route::get('remove-selection/{id},', 'ApiAndroidController@removeSelection');
         Route::post('finalizar-aposta', 'ApiAndroidController@postFinalizarAposta');
 
         /* Rotas para os jogos */
