9cdae7c714ced8e8de77d2c017880d2d76a87e27
Victor Hugo <victorhugoraj@gmail.com>
2021-05-31 23:22:03 -0300

    Update include_sbet.blade.php

--- resources/views/client/include_sbet.blade.php
+++ resources/views/client/include_sbet.blade.php
@@ -29,11 +29,11 @@
   </head>
   <body>
     <div class="ie-panel"><a href="https://windows.microsoft.com/en-US/internet-explorer/"><img src="/images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
-    <div class="preloader">
+    <!-- <div class="preloader">
       <div class="preloader-body">
         <div class="preloader-item"></div>
       </div>
-    </div>
+    </div> -->
     <!-- Page-->
     <style>
 
