60d2e2c3840a3f0faa6cc137beadfd883f003f5d
Victor Hugo <victorhugoraj@gmail.com>
2021-06-06 12:08:44 -0300

    Update AovivoController.php

--- app/Http/Controllers/AovivoController.php
+++ app/Http/Controllers/AovivoController.php
@@ -39,10 +39,9 @@ class AovivoController extends Controller
             do{
                 $response = \Http::get('https://api.b365api.com/v1/events/inplay?sport_id=1&token='.config('app.API_TOKEN'));
                 
-                dd($response);
                 if($response->successful()){
                     $json = json_decode($response->body(), false);
-    
+                    dd($json);
                     if(count($json->results) > 0){
     
     
