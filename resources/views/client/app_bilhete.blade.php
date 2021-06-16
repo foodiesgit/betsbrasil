12de72d1eb2831a77ad7ba5efd454b0f692bdf64
Victor Hugo <victorhugoraj@gmail.com>
2021-06-16 16:22:48 -0300

    Update app_bilhete.blade.php

--- resources/views/client/app_bilhete.blade.php
+++ resources/views/client/app_bilhete.blade.php
@@ -4,6 +4,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{$config->nome_banca}} - {{$cupomAposta->codigo_unico}}</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <!--Custon CSS (está em /public/assets/site/css/certificate.css)-->
