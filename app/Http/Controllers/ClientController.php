fb99a677ff5e76319a8e627fde63f53b3c7211bf
Victor Hugo <victorhugoraj@gmail.com>
2021-06-23 11:12:50 -0300

    Update ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1290,9 +1290,10 @@ public function viewIndex(){
     
                                 if(Auth::user()->idgerente){
                                     $comissaoGerente = GerentesCampos::where('idusuario',Auth::user()->idgerente)->first();
-                                    dd($comissaoGerente);
+           
                                     $porcentagem = $comissaoGerente->comissao / 100;
                                     $comissao = $cupomAposta->valor_apostado * $comissaoGerente->porcentagem;
+                                    dd($comissao);
                                     $credito = Creditos::where('idusuario', Auth::user()->idgerente)->first();
                                     $credito->saldo_liberado =  $credito->saldo_liberado + $comissao;
                                     $credito->save();
