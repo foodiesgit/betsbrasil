a3f3fc3af91e64447ef64b078b4e9820fd3c1168
Victor Hugo <victorhugoraj@gmail.com>
2021-07-10 20:24:36 -0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2814,12 +2814,12 @@ class AdminController extends Controller {
                 });
             }
     
-
+        $p = $sql->get();
 
         $nova_query = $sql->select("events.*", DB::raw("date_format(events.data, '%d/%m/%Y as %H:%i') as data_evento"), "ligas.nome_traduzido as nome_liga", "esportes.nome_traduzido as nome_esporte", "paises.nome as nome_pais", "paises.nome_traduzido as nome_pais_traduzido", DB::raw("sum(valor_apostado) as soma"))
 
         ->orderBy('events.data', 'desc')->take('40')->get();
-        dd($sql, $nova_query);
+        dd($p, $nova_query);
         $data = [
 
             'sql' => $sql,
