1022052bc8a256ded1c195e3c998878ca973d5ba
Victor Hugo <victorhugoraj@gmail.com>
2021-07-10 20:45:17 -0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2814,7 +2814,8 @@ class AdminController extends Controller {
                 });
             }
     
-        $p = $sql->select("events.*", DB::raw("date_format(events.data, '%d/%m/%Y as %H:%i') as data_evento"), "ligas.nome_traduzido as nome_liga","esportes.nome_traduzido as nome_esporte", "paises.nome as nome_pais", "paises.nome_traduzido as nome_pais_traduzido")->get();
+        $p = $sql->select("events.*", DB::raw("date_format(events.data, '%d/%m/%Y as %H:%i') as data_evento"), "ligas.nome_traduzido as nome_liga","esportes.nome_traduzido as nome_esporte", 
+        "paises.nome as nome_pais", "paises.nome_traduzido as nome_pais_traduzido", DB::raw('sum(valor_apostado) as soma'))->get();
   
         // $nova_query = $sql->select("events.*", DB::raw("date_format(events.data, '%d/%m/%Y as %H:%i') as data_evento"), "ligas.nome_traduzido as nome_liga", 
         // , DB::raw("sum(valor_apostado) as soma"))
