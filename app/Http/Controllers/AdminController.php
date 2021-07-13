cff7c822b429344bcf9c21ba08ddbcccc250b2e5
Victor Hugo <victorhugoraj@gmail.com>
2021-07-13 18:15:59 -0300

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1906,7 +1906,7 @@ class AdminController extends Controller {
                         R$ ".number_format($saida,2,',','.')." </span>";
                     })
                     ->addColumn('lancamento', function($row){
-                        $lancamento = 'R$ '.number_format($row->lancamento,2,",",",",".");
+                        $lancamento =  "R$ ".number_format($row->lancamento,2,',','.');
                         return $lancamento;
                     })
                     ->addColumn('comissao', function($row){
