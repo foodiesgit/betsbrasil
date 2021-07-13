5bf515234f671b66c6aad85d999ac3bf0486d488
Victor Hugo <victorhugoraj@gmail.com>
2021-07-13 15:26:50 -0300

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -28,7 +28,7 @@
       <div class="row">
      @if(Auth::user()->tipo_usuario == 1)
 
-        <div class="col-xl-3 col-md-6">
+        <div class="col-xl-2 col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -46,7 +46,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-md-6">
+        <div class="col-xl-2 col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -64,7 +64,7 @@
             </div>
           </div>
         </div>
-        <div class="col-xl-3 col-md-6">
+        <div class="col-xl-2 col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -83,7 +83,7 @@
           </div>
         </div>
       @else
-      <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-3' : 'col-xl-3')}} col-md-6">
+      <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-2' : 'col-xl-2')}} col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -101,7 +101,7 @@
             </div>
           </div>
         </div>
-        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-3' : 'col-xl-3')}} col-md-6">
+        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-2' : 'col-xl-2')}} col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -120,7 +120,7 @@
           </div>
         </div>
         @if(Auth::user()->tipo_usuario == 2 || Auth::user()->tipo_usuario == 3)
-        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-3' : 'col-xl-3')}} col-md-6">
+        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-2' : 'col-xl-2')}} col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -140,7 +140,7 @@
           </div>
         </div>
         @endif
-        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-3' : 'col-xl-3')}} col-md-6">
+        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-2' : 'col-xl-2')}} col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
@@ -159,7 +159,7 @@
             
           </div>
         </div>
-        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-3' : 'col-xl-3')}} col-md-6">
+        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-2' : 'col-xl-2')}} col-md-6">
           <div class="card card-stats">
             <!-- Card body -->
             <div class="card-body">
