d7d173f3e5c23be754522c519f97cebd023a61b9
Victor Hugo <victorhugoraj@gmail.com>
2021-07-13 15:23:58 -0300

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -147,7 +147,7 @@
               <div class="row">
                 <div class="col">
                   <h5 class="card-title text-uppercase text-muted mb-0">Lançamentos</h5>
-                  <span class="h2 font-weight-bold mb-0" ">R$ {{number_format($Lançamento,2,',','.')}}</span>
+                  <span class="h2 font-weight-bold mb-0" ">R$ {{number_format($lançamento,2,',','.')}}</span>
                 </div>
                 <div class="col-auto">
                   <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
