5546a22b87e6741efcb06f896aaf0da3c5bef90d
Victor Hugo <victorhugoraj@gmail.com>
2021-07-17 08:41:20 -0300

    Update view_bilhetes.blade.php

--- resources/views/admin/cambistas/view_bilhetes.blade.php
+++ resources/views/admin/cambistas/view_bilhetes.blade.php
@@ -86,6 +86,25 @@
             <div class="card-body">
             <div class="row">
                 <div class="col">
+                <h5 class="card-title text-uppercase text-muted mb-0">Lançamento</h5>
+                <span class="h2 font-weight-bold mb-0" ">R$ {{number_format($lancamento,2,',','.')}}</span>
+                </div>
+                <div class="col-auto">
+                <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                    <i class="ni ni-money-coins"></i>
+                </div>
+                </div>
+            </div>
+            </div>
+            
+        </div>
+        </div>
+        <div class="col-xl-2 col-md-6">
+        <div class="card card-stats">
+            <!-- Card body -->
+            <div class="card-body">
+            <div class="row">
+                <div class="col">
                 <h5 class="card-title text-uppercase text-muted mb-0">Comissão do Cambista</h5>
                 <span class="h2 font-weight-bold mb-0" ">R$ {{number_format($comissao,2,',','.')}}</span>
                 </div>
