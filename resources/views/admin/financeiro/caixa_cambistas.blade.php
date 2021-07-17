7c858a0562e9587e906909dbb454f04388bef0d5
Victor Hugo <victorhugoraj@gmail.com>
2021-07-17 09:13:12 -0300

    Update caixa_cambistas.blade.php

--- resources/views/admin/financeiro/caixa_cambistas.blade.php
+++ resources/views/admin/financeiro/caixa_cambistas.blade.php
@@ -118,25 +118,6 @@
             
           </div>
         </div>
-        <div class="{{(Auth::user()->tipo_usuario == 4 ? 'col-xl-2' : 'col-xl-2')}} col-md-6">
-          <div class="card card-stats">
-            <!-- Card body -->
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  <h5 class="card-title text-uppercase text-muted mb-0">Total</h5>
-                  <span class="h2 font-weight-bold mb-0"id="saldo3">R$ {{number_format($total,2,',','.')}}</span>
-                </div>
-                <div class="col-auto">
-                  <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
-                    <i class="ni ni-money-coins"></i>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-          </div>
-        </div>
       </div>
     </div>
   </div>
