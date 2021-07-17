f7441f18f16bb42c48c77f19a72d257d96446571
Victor Hugo <victorhugoraj@gmail.com>
2021-07-17 08:30:23 -0300

    Update index.blade.php

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -89,7 +89,7 @@
             <div class="card-body">
               <div class="row">
                 <div class="col">
-                  <h5 class="card-title text-uppercase text-muted mb-0">Entradas</h5>
+                  <h5 class="card-title text-uppercase text-muted mb-0">Entradas Pendentes</h5>
                   <span class="h2 font-weight-bold mb-0" >R$ {{number_format($entradaPendente,2,',','.')}} </span>
                 </div>
                 <div class="col-auto">
